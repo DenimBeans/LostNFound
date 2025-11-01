32532c8404cf083255c6010c71f27673f3e1980a
Lu-Minimum <160276327+Lu-Minimum@users.noreply.github.com>
2025-11-01 18:46:24 -0400

    Created models unit tests
    Created these while working on the more important unit tests

---
+++ backend/__tests__/model.test.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import Item from '../src/models/Item.js';
+import User from '../src/models/User.js';
+
+describe('Model Tests', () => {
+    test('Item model exists', () => {
+        expect(Item).toBeTruthy();
+    });
+    test('User model exists', () => {
+        expect(User).toBeTruthy();
+    });
+    test('Item model can be used', () => {
+        expect(new Item()).toBeInstanceOf(mongoose.Document);
+    });
+    test('User model can be used', () => {
+        expect(new User()).toBeInstanceOf(mongoose.Document);
+    });
+    test('Item model requires title', () => {
+        var tempError = (new Item()).validateSync().errors['title'];
+        expect(tempError).toBeTruthy();
+        expect(tempError.kind).toBe('required');
+    });
+    test('User model requires firstName', () => {
+        var tempError = (new User()).validateSync().errors['firstName'];
+        expect(tempError).toBeTruthy();
+        expect(tempError.kind).toBe('required');
+    });
+    test('User model requires email', () => {
+        var tempError = (new User()).validateSync().errors['email'];
+        expect(tempError).toBeTruthy();
+        expect(tempError.kind).toBe('required');
+    });
+    test('User model requires password', () => {
+        var tempError = (new User()).validateSync().errors['password'];
+        expect(tempError).toBeTruthy();
+        expect(tempError.kind).toBe('required');
+    });
+});
