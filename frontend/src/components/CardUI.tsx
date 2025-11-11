0d54ed3722faff330e95e5a0f84aa278e4b1ae99
Fsch <Shamu2021@outlook.com>
2025-11-10 20:22:10 -0500

    Found actual item api

--- frontend/src/components/CardUI.tsx
+++ frontend/src/components/CardUI.tsx
@@ -130,7 +130,7 @@ function CardUI() {
         //let js = JSON.stringify(obj);
 
         try {
-            const response = await fetch(buildAPIPath(`api/items/:${itemUSERID}`),
+            const response = await fetch(buildAPIPath(`api/users/:${itemUSERID}/items`),
                 { method: 'GET', headers: { 'Content-Type': 'application/json' } });
 
             let txt = await response.text();
