88bd6c6e8b7abcb32a8b6f25da01aec9611f5425
Fsch <Shamu2021@outlook.com>
2025-11-13 22:52:47 -0500

    Testing very basic setup to notification

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -7,14 +7,39 @@ import {useEffect} from 'react';
 
 function Notification(){
 
+    interface NotifData{
+        _id : string;
+        title : string;
+        
+    }
+
+
+    const [NotifContainer,setNotifContainer] = useState<NotifData[]>([]);
+
     const [itemUSERID, setItemUSERIDValue] = React.useState('');
+
+    async function AllNotif(id: any){
+        try {
+            const response = await fetch(buildAPIPath(`api/users/${id}/notifications`),
+                { method: 'GET', headers: { 'Content-Type': 'application/json' } });
+
+            let txt = await response.text();
+            let res = JSON.parse(txt);
+            setNotifContainer(res.results);
+            }
+            
+        
+        catch (error: any) {
+            console.log("Frontend Error");
+        }
+    }
 
     useEffect(() => {
         const Data = sessionStorage.getItem('user_data');
         if(Data != null){
             const UserData = JSON.parse(Data);
             setItemUSERIDValue(UserData?.userId);
-            //searchItem(true);
+            AllNotif(UserData?.userId);
         }
         else{
             window.location.href = '/';
@@ -27,9 +52,17 @@ function Notification(){
 //Remove once itemUSERID is used elsewhere - Jean
     return(
         <div id = "NotificationMain">
-            
-            <p>{itemUSERID}</p>
-            
+
+            <div id = "LostItemPage">
+                {NotifContainer.map(NotifContainer => (
+                    <div key = {NotifContainer.itemId} className = "NotifContainers">
+                        <input type = "text" id = "NotificationTitle" className = "NotTitle"  readOnly/>
+                        <button type = "button" id = "NotificationData" >View Report</button>
+                        <button type = "button" id = "NotificationDelete" >Delete</button>
+                    </div>
+                ))}
+                
+            </div>
             <div id = "buttonholder">
                 <button type = "button" id = "deleteAllNotif">Delete</button>
             </div>
