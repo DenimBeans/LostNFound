4da537994da49bb5b30a9d421f919b958d7b8a61
Fsch <Shamu2021@outlook.com>
2025-11-14 20:27:06 -0500

    Fixed Emailrevver

--- frontend/src/components/EmailReVer.tsx
+++ frontend/src/components/EmailReVer.tsx
@@ -1,8 +1,47 @@
 function EmailReVer(){
+const { token } = useParams();
+const [message, setMessage] = useState('');
 
-    return(
-        <h1>Leave</h1>
-    )
-}
+
+useEffect(() => {Verify(token);},[token]);
+  
+async function Verify(token: any){
+
+   try {
+      
+      const response = await fetch(buildAPIPath(`api/auth/verify/${token}`),
+        { method: 'GET', headers: { 'Content-Type': 'application/json' } });
+
+      var res = JSON.parse(await response.text());
+
+      if (res.success == true) {
+        setMessage(res.message);
+        
+      }
+      if(res.error == "Invalid or expired verification token"){
+        setMessage(res.error);
+      }
+
+      else {
+        setMessage(res.error);
+      }
+    }
+
+    catch (error: any) {
+      alert(error.toString());
+      return;
+    }
+  };
+
+  
+  
+  
+ return (
+    <div id = "EmailMain">
+      <h1>{message} Leave</h1>
+    </div>
+    
+  );
+};
 
 export default EmailReVer
