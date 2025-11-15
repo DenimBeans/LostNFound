b7c6fe49238885c888a464e576655bd86af23182
DenimBeans <thegooseman16@gmail.com>
2025-11-14 22:52:47 -0500

    Changed Logo

--- frontend/lib/home.dart
+++ frontend/lib/home.dart
@@ -91,7 +91,9 @@ class _AppHomeState extends State<AppHome> {
           lastName: widget.lastName,
           email: widget.email,
         ),
-        InboxDisplay(userId: widget.userId),
+        InboxDisplay(
+          userId: widget.userId
+        ),
       ][currentPageIndex],
       endDrawer: Drawer(
         backgroundColor: AppColors.secondaryBackground,
@@ -271,7 +273,7 @@ class Item {
       reporterFName: firstName ?? '',
       reporterLName: lastName ?? '',
       reporterEmail: email ?? '',
-      imageUrl: json['imageUrl'] ?? '',
+      imageUrl: json['imageUrl'] ?? ''
     );
   }
 }
--- frontend/lib/notifications.dart
+++ frontend/lib/notifications.dart
@@ -6,6 +6,19 @@ import 'dart:convert';
 import 'main.dart';
 import 'home.dart';
 
+class Sender
+{
+  final String id;
+  final String firstName;
+  final String lastName;
+
+  Sender({
+    required this.id,
+    required this.firstName,
+    required this.lastName
+  });
+}
+
 //  Notifications Inbox Widgets
 class Notification {
   final String userId;
@@ -14,8 +27,8 @@ class Notification {
   final bool isMeetup;
   final String location;
   final DateTime meetTime;
-  final String? senderId;
-  final int? itemId;
+  final Sender? senderId;
+  final Item? itemId;
 
   Notification({
     required this.userId,
@@ -27,8 +40,8 @@ class Notification {
     this.senderId,
     this.itemId,
   });
-
-  factory Notification.fromJson(Map<String, dynamic> json) {
+  
+  factory Notification.fromJson(json) {
     return Notification(
       userId: json['userId'],
       notifText: json['text'],
@@ -37,15 +50,19 @@ class Notification {
       location: json['location'],
       meetTime: json['meetTime'],
       senderId: json['senderId'],
-      itemId: json['itemId'],
+      itemId: json['itemId']
     );
   }
 }
 
 class InboxDisplay extends StatelessWidget {
+
   final String userId;
 
-  const InboxDisplay({super.key, required this.userId});
+  const InboxDisplay({
+    super.key,
+    required this.userId,
+  });
 
   Future<List<Notification>> get notifsFuture => getNotifs();
 
@@ -54,16 +71,55 @@ class InboxDisplay extends StatelessWidget {
       Uri.parse('http://knightfind.xyz:4000/api/users/${userId}/notifications'),
       headers: {'Content-Type': 'application/json'},
     );
-
+  
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      return data.map((e) => Notification.fromJson(e)).toList();
+      final results = data['results'] as List;
+      return data((e) => Notification.fromJson(e)).toList();
     } else {
       throw Exception('Failed to load user notifications');
     }
   }
 
+  
   @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [          
+          SizedBox(
+            height: 250, 
+            //  To display notifications in ListView
+            child: FutureBuilder<List<Notification>>(
+              future: notifsFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  // until data is fetched, show loader
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (snapshot.hasData) {
+                  // once data is fetched, display it on screen (call buildPosts())
+                  return SizedBox(width: 350, height: 250);
+                } else {
+                  // if no data, show simple Text
+                  return const Text("No data available");
+                }
+              }
+            ),
+          ),
+          const SizedBox(height: 20),
+          InputTextField(label: 'Search Item', isObscure: false),
+          const SizedBox(height: 20),
+          //SizedBox(width: 350, height: 250, child: const SearchMap()),
+          const SizedBox(height: 20),
+          // Search results will be populated from API
+        ],
+      ),
+    );
+  }
+
+  /*@override
   Widget build(BuildContext context) {
     return Center(
       child: FutureBuilder<List<Notification>>(
@@ -83,12 +139,11 @@ class InboxDisplay extends StatelessWidget {
               style: TextStyle(fontSize: 18, color: Colors.grey),
             );
           }
-        },
+        }
       ),
     );
-  }
-
-  // function to display fetched data on screen
+  }*/
+    // function to display fetched data on screen
   Widget tempBuildList(List<Notification> notifs) {
     // ListView Builder to show data in a list
     return ListView.builder(
