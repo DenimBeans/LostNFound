b4b611dd32ad5e766d40d1bdd9656b0f9573ecc8
Fsch <Shamu2021@outlook.com>
2025-11-15 21:05:07 -0500

    More Changes to notification

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -304,9 +304,9 @@ function Notification() {
                     <div key={NotifContainer._id} className="NotifContainers">
                         <input type="text" id="NotificationTitle" className="NotTitle" value={NotifContainer.text} readOnly />
                         <input type="text" id="NotificationMeetup" className="NotTitle" value={NotifContainer.isRead} readOnly />
-                        <button type="button" id="NotificationView" onClick={() => View(NotifContainer)}>View Report</button>
-                        <button type="button" id="NotificationRead" onClick={() => Read(NotifContainer._id)}>Read</button>
-                        <button type="button" id="NotificationDelete" onClick={() => Delete(NotifContainer._id)}>Delete</button>
+                        <button type="button" id="NotificationView" className = "NotIfBtn" onClick={() => View(NotifContainer)}>View Report</button>
+                        <button type="button" id="NotificationRead" className = "NotIfBtn" onClick={() => Read(NotifContainer._id)}>Read</button>
+                        <button type="button" id="NotificationDelete" className = "NotIfBtn" onClick={() => Delete(NotifContainer._id)}>Delete</button>
                     </div>
                 ))}
 
