e65c04b768081bc3f1a72a1b00c8960962912511
Fsch <Shamu2021@outlook.com>
2025-11-16 10:45:44 -0500

    Testing some new css changes to account settings

--- frontend/src/components/AccountSettings.tsx
+++ frontend/src/components/AccountSettings.tsx
@@ -200,10 +200,10 @@ function AccountSettings(){
                 
                 <button type = "button" id = "deleteuserbtn"  onClick = {DeleteUserInfo}>Delete User!</button>
             </div>
-
-            <input type = "text" id = "firstname" className = "AccountSet" value = {userFirst} readOnly/>
-            <input type = "text" id = "lastname" className = "AccountSet" value = {userLast}  readOnly/>
-            <input type = "text" id = "Accountemail" className = "AccountSet" value = {userEmail} readOnly/>
+            
+            <span className = "MainAccountSet" id = "fname">Firstname:<input type = "text" id = "firstname" className = "AccountSet" value = {userFirst} readOnly/></span>
+            <span className = "MainAccountSet" id = "lname">Lastname:<input type = "text" id = "lastname" className = "AccountSet" value = {userLast}  readOnly/></span>
+            <span className = "MainAccountSet" id = "email">Email:<input type = "text" id = "Accountemail" className = "AccountSet" value = {userEmail} readOnly/></span>
             
             
             <div id = "buttonbar">
