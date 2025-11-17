74936e36fd92f5a791e1dd24a03805de6859ed56
Fsch <Shamu2021@outlook.com>
2025-11-17 14:31:39 -0500

    Notification empty fix

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -510,12 +510,11 @@ function Notification() {
                         <button type="button" id="NotificationDelete" className="NotIfBtn" onClick={() => Delete(NotifContainer._id)}></button>
                     </div>
                 ))}
-                <div id="NoNotif" ref={NoNotif}>
-                <img alt="EmptyNotifications" src={Empty} id="EmptyNotifications" />
-            </div>
 
             </div>
-            
+            <div id="NoNotif" ref={NoNotif}>
+                <img alt="EmptyNotifications" src={Empty} id="EmptyNotifications" />
+            </div>
             <div id="ViewNotIf" ref={ViewNotIf}>
                 <button type="button" id="exit" onClick={() => exit()}>X</button>
 
